0b8e9e3ab30344c41b521349779bfe1f09c732ff
scme0 <scott.merchant@octopus.com>
2025-11-15 13:11:48 +0100

    fix bugs, simply interface, expand examples

--- grid.py
+++ grid.py
@@ -1,27 +1,29 @@
-from enum import Enum
+from enum import StrEnum
 from manim import MovingCameraScene, Graph, Text, MovingCamera, Line, DashedLine, Create
-from manim import UP, WHITE, PURPLE, GREEN, YELLOW, RED
+from manim import UP, WHITE, PURPLE, GREEN, YELLOW, RED, GRAY
+from json import dumps
+from collections import namedtuple
 
-class StitchType(Enum):
-    FrontOne = 0
-    FrontTwo = 1
-    BackOne = 2
-    BackTwo = 3
-    Automatic = 4
+def stringify(object):
+    return dumps(object.__dict__)
 
-class Corner(Enum):
-    TopLeft = 0
-    TopRight = 1
-    BottomLeft = 2
-    BottomRight = 3
-        
-class Point:
-    def __init__(self, squareId: int, corner: Corner):
-        self.squareId = squareId
-        self.corner = corner
+class StitchType(StrEnum):
+    FrontOne = "Front1"
+    FrontTwo = "Front2"
+    BackOne = "Back1"
+    BackTwo = "Back2"
+    Automatic = "Automatic"
+
+class Corner(StrEnum):
+    TopLeft = "TopLeft"
+    TopRight = "TopRight"
+    BottomLeft = "BottomLeft"
+    BottomRight = "BottomRight"
+
+StitchPoint = tuple[int, Corner]
 
 class Stitch:
-    def __init__(self, fro: Point, to: Point, type: StitchType):
+    def __init__(self, fro: StitchPoint, to: StitchPoint, type: StitchType):
         self.fro = fro
         self.to = to
         self.type = type
@@ -52,19 +54,19 @@ class Square:
             3+(count*4),
         ]
         self.edges = [
-            (self.corners[Corner.TopLeft], self.corners[Corner.TopRight]),
-            (self.corners[Corner.TopRight], self.corners[Corner.BottomRight]),
-            (self.corners[Corner.BottomRight], self.corners[Corner.BottomLeft]),
-            (self.corners[Corner.BottomLeft], self.corners[Corner.TopLeft])
+            (self.verticies[0], self.verticies[1]),
+            (self.verticies[1], self.verticies[2]),
+            (self.verticies[2], self.verticies[3]),
+            (self.verticies[3], self.verticies[0])
         ]
         self.layouts = {
             self.verticies[0]: self.corners[Corner.BottomLeft],
             self.verticies[1]: self.corners[Corner.BottomRight],
-            self.verticies[2]: self.corners[Corner.TopLeft],
-            self.verticies[3]: self.corners[Corner.TopRight],
+            self.verticies[2]: self.corners[Corner.TopRight],
+            self.verticies[3]: self.corners[Corner.TopLeft],
         }
 
-class Grid:
+class Aida:
     def __init__(self, title: str, x: int, y: int, removed = {}):
         self.title = title
         self.x = x
@@ -88,7 +90,7 @@ class Grid:
             verticies.extend(square.verticies)
             edges.extend(square.edges)
             lt.update(square.layouts)
-        g = Graph(verticies, [], layout=lt)
+        g = Graph(verticies, edges, layout=lt, edge_config={"stroke_color": GRAY, "stroke_width": 1})
         scene.add(g)
         t = Text(self.title, font="Arial", color=WHITE, font_size=24).next_to(g, UP)
         scene.add(t)
@@ -97,29 +99,32 @@ class Grid:
 
         for stitch in self.stiches:
             if isinstance(stitch, Stitch):
-                fromSquare = self.squares[stitch.fro.squareId]
-                toSquare = self.squares[stitch.to.squareId]
-                fromPoint = fromSquare.corners[stitch.fro.corner]
-                toPoint = toSquare.corners[stitch.to.corner]
+                fromSquare = self.squares[stitch.fro[0]]
+                toSquare = self.squares[stitch.to[0]]
+                fromPoint = fromSquare.corners[stitch.fro[1]]
+                toPoint = toSquare.corners[stitch.to[1]]
                 if stitch.type == StitchType.Automatic:
-                    raise KeyError("Stitch cannot use Automatic Stitch type")
+                    raise KeyError("Stitch cannot use Automatic Stitch type", stringify(stitch))
             elif isinstance(stitch, SimpleStitch):
+                to = stitch.to
+                fro = stitch.fro
                 square = self.squares[stitch.squareId]
-                fromPoint = square.corners[stitch.fro]
-                toPoint = square.corners[stitch.to]
+                fromPoint = square.corners[fro]
+                toPoint = square.corners[to]
                 if stitch.type == StitchType.Automatic:
-                    if stitch.fro == Corner.TopLeft and stitch.to == Corner.BottomRight:
+
+                    if (fro == Corner.TopLeft and to == Corner.BottomRight) or (fro == Corner.BottomRight and to == Corner.TopLeft):
                         stitch.type = StitchType.FrontOne
-                    elif stitch.fro == Corner.BottomRight and stitch.to == Corner.TopRight:
+                    elif fro == Corner.BottomRight and (to == Corner.TopRight or to == Corner.BottomLeft):
                         stitch.type = StitchType.BackOne
-                    elif stitch.fro == Corner.TopRight and stitch.to == Corner.BottomLeft:
+                    elif (fro == Corner.TopRight and to == Corner.BottomLeft) or (fro == Corner.BottomLeft and to == Corner.TopRight):
                         stitch.type = StitchType.FrontTwo
-                    elif stitch.fro == Corner.BottomLeft and stitch.to == Corner.TopLeft:
+                    elif (fro == Corner.BottomLeft or fro == Corner.TopRight) and to == Corner.TopLeft:
                         stitch.type = StitchType.BackTwo
                     else:
-                        raise KeyError("Unable to decide which stitch to use for given SimpleStitch", stitch)
+                        raise KeyError("Unable to decide which stitch to use for given SimpleStitch", stringify(stitch))
             else:
-                raise KeyError("Unknown Stitch Type", stitch)
+                raise KeyError("Unknown Stitch Type", stringify(stitch))
             match stitch.type:
                 case StitchType.FrontOne:
                     l = Line(fromPoint, toPoint, color=PURPLE)
